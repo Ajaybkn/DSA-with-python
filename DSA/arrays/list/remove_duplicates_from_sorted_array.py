6d9cc9360806ae1c35ec6d88f296e4a1afcde8a9
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2025-08-22 17:06:08 +0530

    add remove_duplicates function to eliminate duplicates from sorted array

---
+++ DSA/arrays/list/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,27 @@
+def remove_duplicates(arr):
+    dictn = {}
+    for i in range(0, len(arr)):
+        dictn[arr[i]] = 0
+    j = 0
+    for k in dictn:
+        arr[j] = k
+        j += 1
+    return arr[:j]
+
+
+ans = remove_duplicates([1, 1, 1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 6])
+print(ans)
+
+#  2nd Method--->>>
+
+
+# def remove_duplicates(arr):
+#     ans = []
+#     for i in arr:
+#         if i not in ans:
+#             ans.append(i)
+#     return ans
+
+
+# answ = remove_duplicates([1, 1, 1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 6])
+# print(answ)
