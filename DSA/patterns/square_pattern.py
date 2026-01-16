b0d9b63882901b1b3d15b6f0556d2ae33ced1226
AjaysinghTechoon <ajays@techoon.in>
2026-01-16 11:45:44 +0530

    update

--- DSA/patterns/square_pattern.py
+++ DSA/patterns/square_pattern.py
@@ -1,4 +1,4 @@
-def pattern(n):
+def square_pattern(n):
     for i in range(n):
         for j in range(n):
             print('*', end=" ")
@@ -6,4 +6,4 @@ def pattern(n):
 
 
 
-pattern(4)
+square_pattern(4)
